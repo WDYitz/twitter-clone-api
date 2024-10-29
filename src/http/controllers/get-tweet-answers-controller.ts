0ff14c605d3f80348c8d2634bb0c4adc1911e41a
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-10-29 19:48:57 -0300

    refactor: Add get-tweet-answers-controller.ts

--- src/http/controllers/get-tweet-answers-controller.ts
+++ src/http/controllers/get-tweet-answers-controller.ts
@@ -1,23 +1,23 @@
 import { TweetDoesNotExistError } from "@/error_handler/TweetDoesNotExist";
 import { TweetRepository } from "@/repositories/tweets-repository";
 import type { ExtendedRequest } from "@/types/request/extended-requests";
-import { GetTweetUseCase } from "@/use-cases/getTweet-useCase";
+import { GetTweetAnswersUseCase } from "@/use-cases/getTweetAnswers-useCase";
 import { Response } from "express";
 
-export const getTweetController = async (req: ExtendedRequest, res: Response): Promise<any> => {
+export const getTweetAnswersController = async (req: ExtendedRequest, res: Response): Promise<any> => {
   try {
     const { id } = req.params;
 
     if (!id || id === null || id === "") {
       return res.status(400).json({ error: "ID do tweet não informado" });
     }
-    
+
     const tweetRepository = new TweetRepository();
-    const getTweetUseCase = new GetTweetUseCase(tweetRepository);
+    const getTweetAnswersUseCase = new GetTweetAnswersUseCase(tweetRepository);
 
-    const tweet = await getTweetUseCase.execute(parseInt(id));
+    const answers = await getTweetAnswersUseCase.execute(parseInt(id));
 
-    return res.status(200).json({ tweet });
+    return res.json({ answers })
 
   } catch (error) {
     if (error instanceof TweetDoesNotExistError) {
