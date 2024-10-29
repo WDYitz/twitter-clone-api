70c39e49654942043ad2e5bcb12d6ca0dc8c2b14
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-10-29 19:49:09 -0300

    refactor: Add findAnswersFromTweet method to TweetRepositoryInterface

--- src/repositories/interfaces/tweets-interface.ts
+++ src/repositories/interfaces/tweets-interface.ts
@@ -1,7 +1,9 @@
 import type { FindTweetResponseType } from "@/types/request/find-tweet-request";
+import type { GetTweetAnswersResponse } from "@/types/response/get-tweet-answers-response";
 import type { Tweet } from "@prisma/client";
 
 export interface TweetRepositoryInterface {
   findTweet(id: number): Promise<FindTweetResponseType | null>;
   createTweet(slug: string, body: string, answer?: number): Promise<Tweet>;
+  findAnswersFromTweet(id: number): Promise<GetTweetAnswersResponse>;
 }
--- src/repositories/tweets-repository.ts
+++ src/repositories/tweets-repository.ts
@@ -1,9 +1,33 @@
 import type { TweetRepositoryInterface } from "./interfaces/tweets-interface";
 import { db } from "@/lib/prisma";
 import type { FindTweetResponseType } from "@/types/request/find-tweet-request";
+import type { GetTweetAnswersResponse } from "@/types/response/get-tweet-answers-response";
 import type { Tweet } from "@prisma/client";
 
 export class TweetRepository implements TweetRepositoryInterface {
+  async findAnswersFromTweet(id: number): Promise<GetTweetAnswersResponse> {
+    const tweets = await db.tweet.findMany({
+      include: {
+        User: {
+          select: {
+            name: true,
+            avatar: true,
+            slug: true
+          }
+        },
+        likes: {
+          select: {
+            userSlug: true
+          }
+        }
+      },
+      where: {
+        answerOf: id
+      }
+    })
+
+    return tweets;
+  }
   async createTweet(slug: string, body: string, answer?: number): Promise<Tweet> {
     const newTweet = await db.tweet.create({
       data: {
