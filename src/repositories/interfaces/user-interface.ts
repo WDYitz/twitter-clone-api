4766db5fe03515f7d459768e211b51709a3fb886
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-10-31 14:30:49 -0300

    refactor: Add methods to retrieve tweet count, followers count, and following count for a user

--- src/repositories/interfaces/user-interface.ts
+++ src/repositories/interfaces/user-interface.ts
@@ -2,9 +2,14 @@ import { CreateUserRequestType } from "@/types/request/create-user-request";
 import { CreateUserResponseType } from "@/types/response/create-user-response";
 import { FindUserBySlugResponseType } from "@/types/response/find-user-by-slug-response";
 import { UserResponseType } from "@/types/response/user-response";
+import type { Tweet } from "@prisma/client";
 
 export interface UserRepositoryInterface {
   findUserByEmail(email: string): Promise<UserResponseType | null>;
   findUserBySlug(slug: string): Promise<FindUserBySlugResponseType | null>;
   createUser(data: CreateUserRequestType): Promise<CreateUserResponseType>;
+  getUserFollowingCount(slug: string): Promise<number>;
+  getUserFollowersCount(slug: string): Promise<number>;
+  getUserTweetCount(slug: string): Promise<number>;
+  findTweetsByUser(slug: string, currentPage: number, perPage: number): Promise<Tweet[]>;
 }
