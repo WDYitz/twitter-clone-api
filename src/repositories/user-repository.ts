63cfb121152640a789a6da2a9b4144121a00b3d0
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-10-31 14:30:40 -0300

    refactor: Add methods to retrieve tweet count, followers count, and following count for a user

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,11 +1,55 @@
 import { db } from "@/lib/prisma";
-import { getPublicURL } from "@/utils/url";
-import { UserRepositoryInterface } from "./interfaces/user-interface";
-import { UserResponseType } from "@/types/response/user-response";
 import { CreateUserRequestType } from "@/types/request/create-user-request";
 import { FindUserBySlugResponseType } from "@/types/response/find-user-by-slug-response";
+import { UserResponseType } from "@/types/response/user-response";
+import { getPublicURL } from "@/utils/url";
+import { UserRepositoryInterface } from "./interfaces/user-interface";
+import type { FindTweetsByUserResponseType } from "@/types/response/find-tweets-by-user-response";
 
 export class UserRepository implements UserRepositoryInterface {
+  async findTweetsByUser(slug: string, currentPage: number, perPage: number): Promise<FindTweetsByUserResponseType> {
+    const tweets = await db.tweet.findMany({
+      include: {
+        likes: {
+          select: {
+            userSlug: true
+          }
+        }
+      },
+      where: {
+        userSlug: slug, answerOf: 0
+      },
+      orderBy: {
+        createdAt: 'desc'
+      },
+      skip: currentPage * perPage,
+      take: perPage
+    })
+    return tweets
+  }
+  async getUserTweetCount(slug: string): Promise<number> {
+    const count = await db.tweet.count({
+      where: {
+        userSlug: slug
+      }
+    })
+
+    return count
+  }
+  async getUserFollowersCount(slug: string): Promise<number> {
+    const count = await db.follow.count({
+      where: { user2Slug: slug }
+    })
+
+    return count
+  }
+  async getUserFollowingCount(slug: string): Promise<number> {
+    const count = await db.follow.count({
+      where: { user1Slug: slug }
+    })
+
+    return count
+  }
   async findUserByEmail(email: string): Promise<UserResponseType | null> {
     const user = await db.user.findFirst({
       where: {
