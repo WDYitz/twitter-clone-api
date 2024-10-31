03ca37a8b36874754f1809861b72dab995afebef
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-10-31 14:30:57 -0300

    refactor: Add getUserController, getUserTweetsController, and userFollowController

--- src/http/routes/router.ts
+++ src/http/routes/router.ts
@@ -6,7 +6,10 @@ import { signinController } from "@/http/controllers/signin-controller";
 import { getTweetController } from "@/http/controllers/get-tweet-controller";
 import { addTweetController } from "@/http/controllers/add-tweet-controller";
 import { getTweetAnswersController } from "@/http/controllers/get-tweet-answers-controller";
-import { checkIfTweetIsLikedByUserController } from "../controllers/check-tweet-liked-by-user-controller";
+import { checkIfTweetIsLikedByUserController } from "@/http/controllers/check-tweet-liked-by-user-controller";
+import { getUserController } from "@/http/controllers/get-user-controller";
+import { getUserTweetsController } from "@/http/controllers/get-user-tweets-controller";
+import { userFollowController } from "@/http/controllers/user-follow-controller";
 
 export const router = Router();
 
@@ -21,9 +24,9 @@ router.get("/tweet/:id", verifyJWTMiddleware, getTweetController);
 router.get("/tweet/:id/answers", verifyJWTMiddleware, getTweetAnswersController);
 router.post("/tweet/:id/like", verifyJWTMiddleware, checkIfTweetIsLikedByUserController);
 
-// router.get("/user/:slug");
-// router.get("/user/:slug/tweets");
-// router.post("/user/:slug/follow");
+router.get("/user/:slug", verifyJWTMiddleware, getUserController);
+router.get("/user/:slug/tweets", verifyJWTMiddleware, getUserTweetsController);
+router.post("/user/:slug/follow", verifyJWTMiddleware, userFollowController);
 // router.put("/user");
 // router.put("/user/avatar");
 // router.put("/user/cover");
